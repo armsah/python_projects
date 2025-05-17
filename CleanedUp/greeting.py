01c78a7c1068452bf0f4f1d026ee22543ee18e1a
Armen Sahakyan <sahakyanarmen5@gmail.com>
2025-05-18 01:21:44 +0300

    Update greeting.py

--- CleanedUp/greeting.py
+++ CleanedUp/greeting.py
@@ -2,6 +2,6 @@ store_name = "Sarah's Store"
 print(store_name)
 
 hello = "Hello"
-name = input ("Waht's your name?\n")
+name = input ("What's your name?\n")
 greeting = hello +" " +  name
 print(greeting)
